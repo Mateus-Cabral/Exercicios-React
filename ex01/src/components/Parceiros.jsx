b662d127e071ee29533251081b7d9c0dcfc20ebc
Mateus-Cabral <RM93000@fiap.com.br>
2022-08-28 10:48:43 -0300

    Adicionando estilização

--- ex01/src/components/Parceiros.jsx
+++ ex01/src/components/Parceiros.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default ({funcao}) => {
+
+    let btn = {
+        display:'block',
+        margin:'auto',
+        marginTop:'1em'
+    }
+
     return ( 
         <>
             <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde modi sapiente quisquam provident sint iure deleniti officia. Quidem, itaque voluptatem similique cupiditate, enim ipsa numquam possimus fugiat nobis dolore ipsum.</h2>
@@ -8,7 +15,7 @@ export default ({funcao}) => {
             <a href="#">B</a>
             <a href="#">C</a>
             <a href="#">D</a>
-            <button onClick={(e)=>funcao()}>Clique aqui</button>
+            <button style={btn} onClick={(e)=>funcao()}>Clique aqui para ativar um evento no console</button>
         </>
 
      );
